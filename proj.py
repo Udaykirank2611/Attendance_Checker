c99ff31dc85e3876f3dd70e27cb6ca5529c9c658
Uday kiran <76544318+Udaykirank2611@users.noreply.github.com>
2024-07-24 10:57:13 +0530

    proj.py

--- proj.py
+++ proj.py
@@ -3,7 +3,7 @@ st.title("Attendence")
 classs = st.number_input("Eppativarak enni classes ainai")
 atten = st.number_input("Enni attend ainav")
 total = st.number_input("Motham enni classes aitai anukuntunav (350 for csd)")
-min_75 = int(total*0.75)
+min_75 = round(total*0.75)
 remaining = total-classs
 req = min_75-atten
 bunks = remaining-req
@@ -16,4 +16,9 @@ if(st.button('Check attendence')):
         bunks = remaining-req
         st.text("nuv inka {} classes bunk kotochu".format(bunks))
         st.warning("daily clg ra iga")
-
+    st.header("Details")
+    st.text("total number of classes : {}".format(total))
+    st.text("total number of classes took place : {}".format(classs))
+    st.text("total number of classes attended : {}".format(atten))
+    st.text("min number of classes for 75%".format(min_75))
+    st.text("number of classes left : {}".format(remaining))                                                           
