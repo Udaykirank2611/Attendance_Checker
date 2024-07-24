fc076ef063955b23cefbcf50f68d7539edc73a14
Uday kiran <76544318+Udaykirank2611@users.noreply.github.com>
2024-07-24 11:23:39 +0530

    update 1 proj.py

--- proj.py
+++ proj.py
@@ -1,9 +1,10 @@
 import streamlit as st
+import math
 st.title("Attendence")
 classs = st.number_input("Eppativarak enni classes ainai")
 atten = st.number_input("Enni attend ainav")
 total = st.number_input("Motham enni classes aitai anukuntunav (350 for csd)")
-min_75 = round(total*0.75)
+min_75 = math.ceil(total*0.75)
 remaining = total-classs
 req = min_75-atten
 bunks = remaining-req
@@ -20,5 +21,6 @@ if(st.button('Check attendence')):
     st.text("total number of classes : {}".format(total))
     st.text("total number of classes took place : {}".format(classs))
     st.text("total number of classes attended : {}".format(atten))
-    st.text("min number of classes for 75%".format(min_75))
+    st.text("min number of classes for 75% : {}".format(min_75))
     st.text("number of classes left : {}".format(remaining))                                                           
+                                                      
